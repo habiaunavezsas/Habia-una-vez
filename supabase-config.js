cbb6785d20c0de783de356cf9ce078b107a2c301
habia una vez sas <habiaunavezsas17@gmail.com>
2026-09-12 00:21:30 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
   url: 'https://ippicfdrlnwbiivqmvhv.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwcGljZmRybG53YmlpdnFtdmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1Mjc0MzksImV4cCI6MjEwNDEwMzQzOX0.pWOj5PmXOrd1fwVp8N6n99VpGqSWxNTt6epYWgKUiac',
-  adminEmails: ['Habiaunavezsas17@gmail.com']
+  adminEmails: ['Habiaunavezsas17@gmail.com', 'alexgam1029q@gmail.com']
 };
